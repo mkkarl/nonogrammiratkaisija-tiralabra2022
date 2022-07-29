63a06e778c7d2a0e913bb6f0456eaa699d4e6e3a
Mari Karlsson <59450590+mkkarl@users.noreply.github.com>
2022-07-29 23:15:13 +0300

    Lisätty luokka Nonogrammi

---
+++ nonogrammiratkaisija/src/main/java/nonogrammiratkaisija/logiikka/Nonogrammi.java
@@ -0,0 +1,58 @@
+package nonogrammiratkaisija.logiikka;
+
+public class Nonogrammi {
+    private Ruutu[][] ruudukko;
+    private Rivi[] rivit;
+
+    public Nonogrammi(String[] vaakarivit, String[] pystyrivit) {
+        this.ruudukko = new Ruutu[vaakarivit.length][pystyrivit.length];
+        this.rivit = new Rivi[vaakarivit.length + pystyrivit.length];
+
+        for (int i = 0; i < vaakarivit.length; i++) {
+            String[] palat = vaakarivit[i].split(" ");
+            MustaPatka[] patkat = luoPatkat(pystyrivit.length, palat);
+            Vaakarivi rivi = new Vaakarivi(i, patkat);
+            rivit[i] = rivi;
+        }
+
+        for (int i = 0; i < pystyrivit.length; i++) {
+            String[] palat = pystyrivit[i].split(" ");
+            MustaPatka[] patkat = luoPatkat(vaakarivit.length, palat);
+            Pystyrivi rivi = new Pystyrivi(i, patkat);
+            rivit[vaakarivit.length + i] = rivi;
+        }
+    }
+
+    private MustaPatka[] luoPatkat(int rivinPituus, String[] palat) {
+        MustaPatka[] patkat = new MustaPatka[palat.length];
+        int k = palat.length;
+        int[] lb = new int[palat.length];
+        
+        for (int i = 0; i < palat.length; i++) {
+            lb[i] = Integer.valueOf(palat[i]);
+        }
+
+        for (int j = 1; j <= k; j++) {
+            int alku = 0;
+
+            if (j > 1) {
+                for (int i = 1; i <= (j - 1); i++) {
+                    alku += lb[i - 1] + 1;
+                }
+            }
+
+            int loppu = rivinPituus - 1;
+
+            if (j < k) {
+                for (int i = (j + 1); i <= k; i++) {
+                    loppu -= lb[i - 1] + 1;
+                }
+            }
+
+            patkat[j - 1] = new MustaPatka(lb[j - 1], alku, loppu);
+
+        }
+
+        return patkat;
+    }
+}

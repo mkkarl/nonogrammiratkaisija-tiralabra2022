8af6a354b2de432958cbea4d60420b93d02f9a71
Mari Karlsson <59450590+mkkarl@users.noreply.github.com>
2022-08-05 19:28:18 +0300

    luokalle Nonogrammi luotu testejä

--- nonogrammiratkaisija/src/test/java/nonogrammiratkaisija/logiikka/NonogrammiTest.java
+++ nonogrammiratkaisija/src/test/java/nonogrammiratkaisija/logiikka/NonogrammiTest.java
@@ -8,6 +8,14 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class NonogrammiTest {
+
+    Nonogrammi nonogrammi1;
+    Nonogrammi nonogrammi2;
+    Nonogrammi nonogrammi3;
+    Nonogrammi nonogrammi4;
+
+    Nonogrammi nonogrammi6;
+    Nonogrammi nonogrammi7;
     
     public NonogrammiTest() {
 
@@ -23,9 +31,75 @@ public class NonogrammiTest {
 
     @Before
     public void setUp() {
+        String[] yksiValkoinen = new String[1];
+        yksiValkoinen[0] = "";
+        nonogrammi1 = new Nonogrammi(yksiValkoinen, yksiValkoinen);
+
+        String[] yksiMusta = new String[1];
+        yksiMusta[0] = "1";
+        nonogrammi2 = new Nonogrammi(yksiMusta, yksiMusta);
+
+        String[] vaaka3 = {"1","1"};
+        String[] pysty3 = {"2",""};
+        nonogrammi3 = new Nonogrammi(vaaka3, pysty3);
+        nonogrammi4 = new Nonogrammi(pysty3, vaaka3);
+
+        String[] vaaka6 = {"1","","1"};
+        String[] pysty6 = {"1 1"};
+        nonogrammi6 = new Nonogrammi(vaaka6, pysty6);
+        nonogrammi7 = new Nonogrammi(pysty6, vaaka6);
     }
 
     @After
     public void tearDown() {
+    }
+
+    @Test
+    public void konstruktoriLuoRuudukon() {
+        assertNotNull(nonogrammi1.getRuudukko());
+        assertNotNull(nonogrammi2.getRuudukko());
+        assertNotNull(nonogrammi3.getRuudukko());
+        assertNotNull(nonogrammi4.getRuudukko());
+        assertNotNull(nonogrammi6.getRuudukko());
+        assertNotNull(nonogrammi7.getRuudukko());
+    }
+
+    @Test
+    public void ruudukonKorkeusOnOikea() {
+        assertEquals(1, nonogrammi1.getRuudukko().length);
+        assertEquals(1, nonogrammi2.getRuudukko().length);
+        assertEquals(2, nonogrammi3.getRuudukko().length);
+        assertEquals(2, nonogrammi4.getRuudukko().length);
+        assertEquals(3, nonogrammi6.getRuudukko().length);
+        assertEquals(1, nonogrammi7.getRuudukko().length);
+    }
+
+    @Test public void ruudukonLeveysOnOikea() {
+        assertEquals(1, nonogrammi1.getRuudukko()[0].length);
+        assertEquals(1, nonogrammi2.getRuudukko()[0].length);
+        assertEquals(2, nonogrammi3.getRuudukko()[0].length);
+        assertEquals(2, nonogrammi4.getRuudukko()[0].length);
+        assertEquals(1, nonogrammi6.getRuudukko()[0].length);
+        assertEquals(3, nonogrammi7.getRuudukko()[0].length);
+    }
+
+    @Test
+    public void konstruktoriLuoRivit() {
+        assertNotNull(nonogrammi1.getRivit());
+        assertNotNull(nonogrammi2.getRivit());
+        assertNotNull(nonogrammi3.getRivit());
+        assertNotNull(nonogrammi4.getRivit());
+        assertNotNull(nonogrammi6.getRivit());
+        assertNotNull(nonogrammi7.getRivit());
+    }
+
+    @Test
+    public void rivitPituusOnOikea() {
+        assertEquals(2, nonogrammi1.getRivit().length);
+        assertEquals(2, nonogrammi2.getRivit().length);
+        assertEquals(4, nonogrammi3.getRivit().length);
+        assertEquals(4, nonogrammi4.getRivit().length);
+        assertEquals(4, nonogrammi6.getRivit().length);
+        assertEquals(4, nonogrammi7.getRivit().length);
     }
 }
